8cbdd5fd9a2d6ed735495b2b3986f83b71ac7a93
Dylan <dylan_21174@hotmail.com>
2025-07-17 14:41:49 -0300

    upload button impl

--- index.js
+++ index.js
@@ -12,28 +12,27 @@ const s3 = new AWS.S3();
 const upload = multer({ dest: 'uploads/' });
 
 app.post('/upload', upload.single('photo'), async (req, res) => {
-  const fileContent = fs.readFileSync(req.file.path);
+    const fileContent = fs.readFileSync(req.file.path);
 
-  const params = {
-    Bucket: process.env.S3_BUCKET,
-    Key: req.file.originalname,
-    Body: fileContent,
-    ContentType: req.file.mimetype,
-  };
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: req.file.originalname,
+        Body: fileContent,
+        ContentType: req.file.mimetype,
+    };
 
-  try {
-    const data = await s3.upload(params).promise();
-    res.send({ url: data.Location });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error uploading to S3');
-  }
+    try {
+        const data = await s3.upload(params).promise();
+        res.send({ url: data.Location });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error uploading to S3');
+    }
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
+    console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from AWS APIs!');
-});
+app.use(express.static('public'));
+
